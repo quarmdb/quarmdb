068c49b8671bd1b93015efb1e2031493f909e9cd
Joshua Julian <joshuawjulian@gmail.com>
2023-09-21 11:22:26 -0400

    moved database - testing

--- src/lib/conn.ts
+++ src/lib/conn.ts
@@ -2,7 +2,7 @@ import Database from 'better-sqlite3';
 let options: Database.Options = { verbose: console.log, readonly: true };
 let db;
 try {
-	db = new Database('./db/quarm.sqlite3', options);
+	db = new Database('/db/quarm.sqlite3', options);
 	db.pragma('journal_mode = WAL');
 } catch (e) {
 	console.error(e);
