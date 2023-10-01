4d2db0a779c3afd5800fbfa88cfd94713c937599
Joshua Julian <joshuawjulian@gmail.com>
2023-10-01 15:28:33 -0400

    MORE SPAM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
-<p>
-	Well it was nice playing with you all. Project Quarm has lost another active player due to server
-	incompetence. Don't take me the wrong way, you guys have something great going on here. It is just
-	unfortunate that you can't fix the constant crashes on Project Quarm . At the end of the day I am
-	at least glad this happened before I donated the $100 I put aside to support this project. Oh well
-	sh*t happens and life is not fair, cheers to all of you guys on Project Quarm and thanks for the
-	cheese, catch ya later.
-</p>
+<div>
+	<p>
+		Well it was nice playing with you all. Project Quarm has lost another active player due to
+		server incompetence. Don't take me the wrong way, you guys have something great going on here.
+		It is just unfortunate that you can't fix the constant crashes on Project Quarm . At the end of
+		the day I am at least glad this happened before I donated the $100 I put aside to support this
+		project. Oh well sh*t happens and life is not fair, cheers to all of you guys on Project Quarm
+		and thanks for the cheese, catch ya later.
+	</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+	p {
+		width: 100%;
+		font-size: xx-large;
+	}
+</style>
