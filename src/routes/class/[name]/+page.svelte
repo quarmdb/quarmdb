86fe2259b31192d03572238a365f73a632bd804d
Joshua Julian <joshuawjulian@gmail.com>
2023-10-02 10:51:36 -0400

    sfasdfa

--- src/routes/class/[name]/+page.svelte
+++ src/routes/class/[name]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { SpellGlobalsSchema } from '$lib/schema';
-	import type { PageData } from './$types';
+	import type { PageData } from '../$types';
 	export let data: PageData;
 </script>
 
