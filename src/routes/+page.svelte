434899ebe9fa244b8d49dd9de209ec10834bb89d
Joshua Julian <joshuawjulian@gmail.com>
2023-10-03 06:28:15 -0400

    Commits arnt showing?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,7 @@
 <div>
-	<p>
-		Well it was nice playing with you all. Project Quarm has lost another active player due to
-		server incompetence. Don't take me the wrong way, you guys have something great going on here.
-		It is just unfortunate that you can't fix the constant crashes on Project Quarm . At the end of
-		the day I am at least glad this happened before I donated the $100 I put aside to support this
-		project. Oh well sh*t happens and life is not fair, cheers to all of you guys on Project Quarm
-		and thanks for the cheese, catch ya later. <br />-Druid<br />
-		<a
-			href="https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454"
-			>https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454</a
-		>
-	</p>
+	<a href="https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454"
+		>https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454</a
+	>
 </div>
 
 <style>
