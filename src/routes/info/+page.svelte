9b8d8cdafa3d8e261b0593feabbab19e6497948a
Joshua Julian <joshuawjulian@gmail.com>
2023-10-13 08:06:02 -0400

    code linking

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,23 @@
+<div>
+	<h1>Pet's Impact to Experience - Solo</h1>
+	<a
+		href="https://github.com/SecretsOTheP/EQMacEmu/blob/0ede14c26b62a4dfb235679b8cd96bcef9f9521d/zone/exp.cpp#L216"
+		>Link to Code</a>
+	<p>If a pet does > 50% of damage to an NPC - the pet takes half the exp.</p>
+	<h1>Pet's Impact to Experience - Group</h1>
+	<a
+		href="https://github.com/SecretsOTheP/EQMacEmu/blob/0ede14c26b62a4dfb235679b8cd96bcef9f9521d/zone/exp.cpp#L963"
+		>Link to Code</a>
+	<p>
+		If a pet does > 50% of damage to an NPC - the pet is treated as a group
+		member for experience split
+	</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+	}
+</style>
