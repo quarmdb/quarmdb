cd6efb6f21d3a8bfb2e1a9b7d7435c8ad143cd45
Joshua Julian <joshuawjulian@gmail.com>
2023-10-16 18:23:07 -0400

    dfgsf

--- src/lib/components/LoginRegisterForm.svelte
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users ( id bigserial primary key, email text not null,
-email_validation_code text not null, email_validated boolean default false,
-password_hash text not null, salt text not null, refresh text not null,
-allowed_to_login boolean default true, created_at TIMESTAMP DEFAULT NOW() );
