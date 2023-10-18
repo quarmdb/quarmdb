bd2798f0948dfc5fddbddd3f1ce3e805c205f4ef
Joshua W Julian <joshuawjulian@gmail.com>
2023-10-18 16:33:33 +0000

    Working on CSRF stuff

---
+++ src/lib/components/LoginRegisterForm.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<form>
+	<section class="input-group">
+		<label for="Email">Email</label>
+		<input type="email" name="email" />
+	</section>
+</form>
+
+<style>
+	form {
+		display: flex;
+		gap: 1rem;
+		width: 100%;
+		flex-direction: column;
+	}
+	section.input-group {
+		width: 100%;
+		display: flex;
+		position: relative;
+		flex-direction: column;
+		z-index: 2;
+	}
+</style>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { PageData, ActionData } from './$types';
+
+	export let data: PageData;
+
+	export let form: ActionData;
+</script>
+
+{#if form?.success}
+	<!-- this message is ephemeral; it exists because the page was rendered in
+		   response to a form submission. it will vanish if the user reloads -->
+	<p>Successfully logged in! Welcome back</p>
+{:else}
+	<form action="?/register" method="POST">
+		<section class="input-group">
+			<label for="Email">Email</label>
+			<input type="text" name="email" value={form?.email ?? ''} />
+			{#if form?.badEmail}
+				<p>Not Well Formed Email</p>
+			{/if}
+		</section>
+		<input type="submit" value="submit" />
+	</form>
+{/if}
