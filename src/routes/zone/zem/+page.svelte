7e879ad15df00c2f6e1ec9a26fba18ed7d41c03e
Joshua Julian <joshuawjulian@gmail.com>
2023-10-22 05:29:43 -0400

    Zem Table Sortable Test

--- src/routes/zone/zem/+page.svelte
+++ src/routes/zone/zem/+page.svelte
@@ -1,23 +1,45 @@
 <script lang="ts">
+	import { getExpansionByNumber } from '$lib/db/constants';
 	import type { ZoneType } from '$lib/schema';
-	import { debounce } from '$lib/utils';
 	import type { PageData } from './$types';
 	export let data: PageData;
+
+	import SvelteTable from 'svelte-table';
+
+	const rows = data.zones;
+	const columns = [
+		{
+			key: 'long_name',
+			title: 'Long Name',
+			value: (v: ZoneType) => v.long_name,
+			sortable: true
+		},
+		{
+			key: 'short_name',
+			title: 'Short Name',
+			value: (v: ZoneType) => v.short_name,
+			sortable: true
+		},
+		{
+			key: 'zem',
+			title: 'Zem',
+			value: (v: ZoneType) => v.zone_exp_multiplier,
+			sortable: true
+		},
+		{
+			key: 'expansion',
+			title: 'Expansion',
+			value: (v: ZoneType) => getExpansionByNumber(v.expansion) || 'Special',
+			sortable: true,
+			filterOptions: [
+				getExpansionByNumber(1),
+				getExpansionByNumber(2),
+				getExpansionByNumber(3),
+				getExpansionByNumber(4),
+				getExpansionByNumber(5)
+			]
+		}
+	];
 </script>
 
-<table>
-	<tr><th>Zone Long Name</th><th>Zone Short name</th><th>Zone Zem</th></tr>
-	{#each data.zones as row}
-		<tr>
-			<td class="cell">{row.long_name}</td>
-			<td class="cell">{row.short_name}</td>
-			<td class="cell">{row.zone_exp_multiplier}</td>
-		</tr>
-	{/each}
-</table>
-
-<style>
-	table {
-		width: 100%;
-	}
-</style>
+<SvelteTable {rows} {columns} />
