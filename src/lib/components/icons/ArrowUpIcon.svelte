5347a8ed1c98fcce6aeb99d429ed30ed8b3bb2b2
Joshua Julian <joshuawjulian@gmail.com>
2023-10-25 19:31:03 -0400

    Issues

---
+++ src/lib/components/icons/ArrowUpIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<svg
+	class="w-6 h-6 text-gray-800 dark:text-white"
+	aria-hidden="true"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 10 14">
+	<path
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		d="M5 13V1m0 0L1 5m4-4 4 4" />
+</svg>
