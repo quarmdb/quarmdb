b05e0b7c58ae50cc5aefad8487e433c0c0a029b5
Calvin <11654917+calvinmorett@users.noreply.github.com>
2023-11-06 12:32:02 -0500

    reorg-css-styling :: Update +page.svelte
    reorg-css-styling ::
    Moving to single file, to manage styles.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,4 @@
 <div>
-	<a href="https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454"
-		>Meme Posting</a
-	>
+	<a href="https://discord.com/channels/1133452007412334643/1133453587675103323/1158158969349607454">Meme Posting</a>
 </div>
 
-<style>
-	div {
-		display: flex;
-		width: 100%;
-		height: 100%;
-		align-items: center;
-		justify-content: center;
-	}
-</style>
